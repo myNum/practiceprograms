e5dd6bd73ac53db30f5620acce020c5d0a4b2e06
fargofremling <alicia.fremling@gmail.com>
2015-02-12 22:19:35 -0800

    Updated Palindrome Tester

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,18 @@
+# This is a program that tests whether a word is a palindrome or not.
 print "A palindrome is a word, phrase, or sequence that reads the same backward as forward."
 
-user_input = raw_input("What word would you like to test? ").lower()
+user_input = raw_input("What word would you like to test? ")
 
-reverse = user_input[::-1]
+palindrome_test = user_input.lower()
 
-if user_input == reverse:
+reverse = palindrome_test[::-1]
+
+if palindrome_test == reverse:
     isPalindrome = True
 else:
     isPalindrome = False
 
 if isPalindrome:
-    print "Well, would you look at that; it's a palindrome!"
+    print "Well, would you look at that;", user_input, "is a palindrome!"
 else:
-    print "Sorry, Charlie, it's not a palindrome."
+    print "Sorry, Charlie,", user_input, "is not a palindrome."
