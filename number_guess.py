d406dadb231b42116527a4b9fde525a6a1a13096
fargofremling <alicia.fremling@gmail.com>
2015-02-14 13:11:22 -0800

    Add repeat play functionality

--- number_guess.py
+++ number_guess.py
@@ -1,17 +1,27 @@
 import random
 
-comp_number = random.randint(1, 10)
-
-user_number = int(raw_input("I'm thinking of a number between 1 and 10. Take a guess. \n"))
-
-while user_number != comp_number:
+def number_guess():
+    comp_number = random.randint(1, 10)
+    user_number = int(raw_input("I'm thinking of a number between 1 and 10. Take a guess. \n"))
+    while user_number != comp_number:
     
-    if user_number < comp_number:
-        print "No way, Jose, that is too low."
-        user_number = int(raw_input("Guess again. \n"))
+        if user_number < comp_number:
+            print "No way, Jose, that is too low."
+            user_number = int(raw_input("Guess again. \n"))
 
-    if user_number > comp_number:
-        print "Woah, that is too high."
-        user_number = int(raw_input("Try again. \n"))
-else:
-    print "WINNER! You guessed correctly"
+        if user_number > comp_number:
+            print "Woah, that is too high."
+            user_number = int(raw_input("Try again. \n"))
+    else:
+            print "WINNER! You guessed correctly"
+
+    play_again = raw_input("Would you like to play again? Y or N \n")
+
+    if play_again in ["y", "Y"]:
+        number_guess()
+
+    else:
+        print "Until next time!"
+
+number_guess()
+
