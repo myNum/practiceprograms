5cdc02a83b4bb659cde22d70a06c216121baab95
fargofremling <alicia.fremling@gmail.com>
2015-02-17 22:43:13 -0800

    Add support for ValueError

--- number_guess.py
+++ number_guess.py
@@ -1,37 +1,44 @@
 import random
 
 def number_guess():
-    comp_number = random.randint(1, 10)
-    user_number = int(raw_input("I'm thinking of a number between 1 and 10. Take a guess. \n"))
+    while True:
+        comp_number = random.randint(1, 10)
 
-    while user_number != comp_number:
-    
-        if user_number < comp_number and user_number < 1:
-            print "You're not good with instructions, are you? Your guess is BELOW the range of 1 to 10."
-            user_number = int(raw_input("Guess a number that is 1 or above, and 10 or below. \n"))
+        while True:
+            try:
+                user_number = int(raw_input("I'm thinking of a number between 1 and 10. Take a guess. \n"))
 
-        elif user_number < comp_number:
-            print "No way, Jose, that is too low."
-            user_number = int(raw_input("Guess again. \n"))
+                while user_number != comp_number:
+                    if user_number < 1:
+                        print "You're not good with instructions, are you? Your guess is BELOW the range of 1 tO 10."
+                        user_number = int(raw_input("Guess a number that is 1 or above, and 10 or below. \n"))
 
-        if user_number > 10:
-            print "Yo! Learn to read. Your guess is ABOVE the range 1 to 10."
-            user_number = int(raw_input("Guess a number that is 10 or below, and 1 or above. \n"))
+                    elif user_number < comp_number:
+                        print "No way, Jose, that is too low."
+                        user_number = int(raw_input("Guess again. \n"))
 
-        elif user_number > comp_number:
-            print "Woah, that is too high."
-            user_number = int(raw_input("Try again. \n"))
+                    elif user_number > 10:
+                        print "Yo! Learn to read. Your guess is ABOVE the range 1 to 10."
+                        user_number = int(raw_input("Guess a number that is 10 or below, and 1 or above. \n"))
 
-    else:
-        print "WINNER! You guessed correctly"
+                    elif user_number > comp_number:
+                        print "Woah, that is too high."
+                        user_number = int(raw_input("Try again. \n"))
+                else:
+                    print "WINNER! You guessed correctly."
+                    break
 
-    play_again = raw_input("Would you like to play again? Y or N \n")
+            except ValueError:
+                print "Dude, enter a NUMBER!"
+                continue
 
-    if play_again in ["y", "Y"]:
-        number_guess()
+        play_again = raw_input("Would you like to play again? Y or N \n")
 
-    else:
-        print "Until next time!"
+        if play_again in ["y", "Y", "yes", "YES"]:
+            continue
+
+        else:
+            print "Until next time!"
+            break
 
 number_guess()
-
