a3690a4df5841a832eada7082fb8dda9ef0c41d1
fargofremling <alicia.fremling@gmail.com>
2015-02-18 23:51:45 -0800

    Add 'test again' option

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,33 @@
 # This is a program that tests whether a word is a palindrome or not.
-print "A palindrome is a word, phrase, or sequence that reads the same backward as forward."
+print "A palindrome is a word, phrase, or sequence that reads the same backward as forward. \n"
 
-user_input = raw_input("What word would you like to test? ")
+def palindrome():
 
-palindrome_test = user_input.lower()
+    while True:
+        user_input = raw_input("What word would you like to test? ")
 
-reverse = palindrome_test[::-1]
+        palindrome_test = user_input.lower()
 
-if palindrome_test == reverse:
-    isPalindrome = True
-else:
-    isPalindrome = False
+        reverse = palindrome_test[::-1]
 
-if isPalindrome:
-    print "Well, would you look at that;", user_input, "is a palindrome!"
-else:
-    print "Sorry, Charlie,", user_input, "is not a palindrome."
+        if palindrome_test == reverse:
+            isPalindrome = True
+
+        else:
+            isPalindrome = False
+
+        if isPalindrome:
+            print "Well, would you look at that;", user_input, "is a palindrome! \n"
+        else:
+            print "Sorry, Charlie,", user_input, "is not a palindrome. \n"
+
+        play_again = raw_input("Would you like to test another word? Y or N \n")
+    
+        if play_again in ["y", "Y", "yes", "YES"]:
+            continue
+        
+        else:
+            print "Ciao!"
+            break
+
+palindrome()
