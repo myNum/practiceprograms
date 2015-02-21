61f337d4171606e8453f08bfd6def7478ff79b5b
fargofremling <alicia.fremling@gmail.com>
2015-02-20 18:10:12 -0800

    Add another question option.

--- magic_8_ball.py
+++ magic_8_ball.py
@@ -4,25 +4,39 @@ print "Give me a quarter, and I'll tell you your fortune."
 
 question = raw_input("Or you could just ask a question. \n> ")
 
-response = ["Huh?",
-            "Speak up! I can't hear you!",
-            "Dumb question. Try again.",
-            "Reception poor. Ask again.",
-            "You really don't want to know the answer, so I'm just going to pretend you didn't ask that.",
-            "Maybe?",
-            "Hell yes!",
-            "Sure",
-            "Yepper!",
-            "You know it!",
-            "If you're lucky.",
-            "Green means go.",
-            "Eh",
-            "Could be.",
-            "Fuck no.",
-            "No, no, no.",
-            "Yeah, and monkeys might fly out of my butt.",
-            "Outlook not so good. Actually, outlook quite awful.",
-            "Doubtful",
-            "BINGO!"]
+def magic_eight():
+    while True:
+            response = ["Huh?",
+                        "Speak up! I can't hear you!",
+                        "BINGO!",
+                        "Dumb question.",
+                        "Reception poor. Try again later.",
+                        "You really don't want to know the answer, so I'm just going to pretend you didn't ask that.",
+                        "Maybe?",
+                        "Hell yes!",
+                        "Sure",
+                        "Yepper!",
+                        "You know it!",
+                        "If you're lucky.",
+                        "Green means go.",
+                        "Eh",
+                        "Could be.",
+                        "Fuck no.",
+                        "No, no, no.",
+                        "Yeah, and monkeys might fly out of my butt.",
+                        "Outlook not so good. Actually, outlook quite awful.",
+                        "Doubtful",]
+            
+            print (random.choice(response))
+                    
+            another_question = raw_input("Would you like ask another question? Y or N \n")
+                    
+            if another_question in ["y", "Y", "yes", "YES"]:
+                question_two = raw_input("Ask away! \n> ")
+                continue
+                        
+            else:
+                print "Later alligator!"
+                break
 
-print (random.choice(response))
+magic_eight()
