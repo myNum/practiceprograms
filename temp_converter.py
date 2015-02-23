7a344b118d70ac4225286460296d7deab8fecef7
fargofremling <alicia.fremling@gmail.com>
2015-02-22 23:18:22 -0800

    Improve ValueError handling

--- temp_converter.py
+++ temp_converter.py
@@ -1,7 +1,15 @@
 # This program allows users to convert the temperature from Celsius to Fahrenheit and vice versa.
 
 def get_temp():
-    temp = int(raw_input("What temperature would you like to convert? \n> "))
+    while True:
+        try:
+            temp = int(raw_input("What temperature would you like to convert? \n> "))
+            break
+
+        except ValueError:
+            print "Your entry must be a number."
+            continue
+
     return temp
 
 def temp_converter():
@@ -10,26 +18,20 @@ def temp_converter():
 
         c_or_f = raw_input("Would you like to convert to Celsius or to Fahrenheit? (C or F) \n> ").lower()
         
-        try:
-            if c_or_f in ["c", "celsius"]:
-                # c = 5 / 9 * (f - 32)
-                converted_temp = 5.0 / 9.0 * (get_temp() - 32)
-                print "The temperature in Celsius is", converted_temp, "degrees."
-                break
+        if c_or_f in ["c", "celsius"]:
+            # c = 5 / 9 * (f - 32)
+            converted_temp = 5.0 / 9.0 * (get_temp() - 32)
+            print "The temperature in Celsius is", converted_temp, "degrees."
+            break
     
-            elif c_or_f in ["f", "fahrenheit"]:
-                # f = 9 / 5 * C + 32
-                converted_temp = 9.0 / 5.0 * get_temp() + 32
-                print "The temperature in Fahrenheit is", converted_temp, "degrees."
-                break
+        elif c_or_f in ["f", "fahrenheit"]:
+            # f = 9 / 5 * C + 32
+            converted_temp = 9.0 / 5.0 * get_temp() + 32
+            print "The temperature in Fahrenheit is", converted_temp, "degrees."
+            break
         
-            else:
-                print "Please choose C for Celsius or F for Fahrenheit."
-                # continue
-                # This still works with "continue" commented out. Why?
-
-        except ValueError:
-            print "Your entry must be a number."
+        else:
+            print "Please choose C for Celsius or F for Fahrenheit."
             # continue
             # This still works with "continue" commented out. Why?
             
@@ -42,5 +44,3 @@ def temp_converter():
         print "Thanks for converting!"
 
 temp_converter()
-
-# Items remaining include: If ValueError, determine a way to ask the user for the temperature and not start back over at chose between c_or_f. I've tried puting c_or_f above the while True, but if the program runs "else", the program returns a continuous loop of "Please choose C for ..." requiring a keyboard interuption to quit the program.
