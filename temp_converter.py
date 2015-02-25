869e1ae9bbe37572569c74600d9f3e1650cb04a9
fargofremling <alicia.fremling@gmail.com>
2015-02-24 22:50:07 -0800

    Clean up formatting

--- temp_converter.py
+++ temp_converter.py
@@ -1,7 +1,6 @@
 # This program allows users to convert the temperature from Celsius to Fahrenheit and vice versa.
 
 def get_temp():
-
     while True:
         temp = raw_input("What temperature would you like to convert? \n> ")
 
@@ -11,35 +10,33 @@ def get_temp():
         except ValueError:
             print "Your entry must be a number."
 
+
 def temp_converter():
+    converted_temp = None
 
-    while True:
-
+    while converted_temp is None:
         c_or_f = raw_input("Would you like to convert to Celsius or to Fahrenheit? (C or F) \n> ").lower()
-        
+
         if c_or_f in ["c", "celsius"]:
             # c = 5 / 9 * (f - 32)
             converted_temp = 5.0 / 9.0 * (get_temp() - 32)
             print "The temperature in Celsius is", converted_temp, "degrees."
-            break
 
         elif c_or_f in ["f", "fahrenheit"]:
             # f = 9 / 5 * C + 32
             converted_temp = 9.0 / 5.0 * get_temp() + 32
             print "The temperature in Fahrenheit is", converted_temp, "degrees."
-            break
-        
+
         else:
             print "Please choose C for Celsius or F for Fahrenheit."
-            # continue
-            # This still works with "continue" commented out. Why?
-            
+
     another_temp = raw_input("Would you like to calculate another temperature? (Y or N) \n> ").lower()
 
     if another_temp in ["y", "yes"]:
         temp_converter()
-    
+
     else:
         print "Thanks for converting!"
+
 
 temp_converter()
