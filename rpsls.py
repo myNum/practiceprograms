41b28a77244c69029adb70a3f7334f00c27fec2d
fargofremling <alicia.fremling@gmail.com>
2015-03-02 23:16:42 -0800

    Larger function into smaller functions

--- rpsls.py
+++ rpsls.py
@@ -14,18 +14,18 @@ print "This is the more advanced version of rock, paper, scissors. This game als
 import random
 
 def name_to_number(name):
-    if name is "rock":
+    if name == "rock":
         return 0
-    elif name is "Spock":
+    elif name in ["Spock", "spock"]:
         return 1
-    elif name is "paper":
+    elif name == "paper":
         return 2
-    elif name is "lizard":
+    elif name == "lizard":
         return 3
-    elif name is "scissors":
+    elif name == "scissors":
         return 4
     else:
-        print "That is not an acceptable option for RPSLS"
+        print "That is not an acceptable option for RPSLS. Try again."
 
 def number_to_name(number):
     if number is 0:
@@ -41,21 +41,23 @@ def number_to_name(number):
     else:
         print "That is not an acceptable option for RPSLS"
 
-
-def rpsls():
-    # print out the message for the player's choice
+def player_shoots():
     player_choice = raw_input("What would you like to play?\n")
     print "You chose:", player_choice
-    # convert the player's choice to player_number using the function name_to_number()
     player_number = name_to_number(player_choice)
+    return  player_number
+
+def computer_shoots():
     # compute random guess for comp_number using random.randrange()
     comp_number = random.randrange(0,5)
     # convert comp_number to comp_choice using the function number_to_name()
     comp_choice = number_to_name(comp_number)
     # print out the message for computer's choice
-    print "Computer chooses:", comp_choice
-    # compute difference of comp_number and player_number modulo five
-    difference = comp_number - player_number
+    print "Computer chose:", comp_choice
+    return comp_number
+
+def rpsls():
+    difference = (player_shoots() - computer_shoots()) % 5
     # use if/elif/else to determine winner, print winner message
     if difference == 0:
         print "Tie!"
@@ -63,7 +65,10 @@ def rpsls():
         print "You wins!"
     elif difference > 2:
         print "Ah, bummer; computer wins!"
-
-# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+    play_again = raw_input("Would you like to play again? Y or N \n").lower()
+    if play_again in ["y", "yes"]:
+        rpsls()
+    else:
+        print "Thanks for playing!"
 rpsls()
 
