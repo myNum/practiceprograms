ad244047c70d35fab21ef7ffb2275a53e6e762b7
fargofremling <alicia.fremling@gmail.com>
2015-03-04 23:24:51 -0800

    The start of a Pythagorean Triple checker

---
+++ pyth_trip_check.py
@@ -0,0 +1,11 @@
+
+# Create a program that allows the user to input the sides of any triangle, and then return whether the triangle is a Pythagorean Triple or not.
+# Extra Credit
+# If your program requires users to input the sides in a specific order, change the coding so the user can type in the sides in any order. Remember, the hypotenuse (c) is always the longest side.
+# Loop the program so the user can use it more than once without having to restart the program.
+
+def pyth_check(a, b, c):
+    a = raw_input("What is the first side of the triangle you want to check?\n ")
+    b = raw_input("The second side?\n ")
+    c = raw_input("And the third side? n")
+
