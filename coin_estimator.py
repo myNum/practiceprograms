e6f5e8f1226cf899cc0bf970ba0171f283d31440
fargofremling <alicia.fremling@gmail.com>
2015-03-18 23:54:36 -0700

    Initial planning for coin estimator program.

---
+++ coin_estimator.py
@@ -0,0 +1,25 @@
+# Goal Create a program that allows the user to input the total weight of each type of coin they have (pennies, nickels, dimes, and quarters), and then print out how many of each type of wrapper they would need, how many coins they have, and the estimated total value of all of their money.
+# Weight of each coin and how many fit inside each type of wrapper.
+# Subgoals
+# Round all numbers printed out to the nearest whole number.
+# Allow the user to select whether they want to submit the weight in either grams or pounds.
+
+# Penny = 2.5g, 50 per roll, $0.50
+# Nickel = 5.0g, 40 per roll, $2.00
+# Dime = 2.268g, 50 per roll, $5.00
+# Quarter = 5.67g, 40 per roll, $10.00
+
+weight_type = raw_input("Would you like to use grams or pounds as your weight unit?").lower
+
+if weight_type in [gram, grams, g]:
+if weight_type in [pound, pounds, lb, lbs]:
+
+penny_weight =
+nickel_weight =
+dime_weight =
+quarter_weight =
+
+penny_wrappers =
+nickel_wrappers =
+dime_wrappers =
+quarter_wrappers =
